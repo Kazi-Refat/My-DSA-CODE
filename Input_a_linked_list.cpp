743c1287982736f90e3357c6394b41ed6fc7198e
Kazi-Rifat <kazirifat223@gmail.com>
2026-07-02 10:26:30 +0600

    Added Input a linked list code

---
+++ Input_a_linked_list.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        this->data = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+  Node *newnode=new Node(val);
+  if(head==NULL)
+  {
+    head=newnode;
+    tail=newnode;
+    return;
+  }
+  tail->next=newnode;
+  tail=newnode;
+}
+void print_linked_list(Node *head)
+{
+    Node *curr_node = head;
+    while (curr_node != NULL)
+    {
+        cout << curr_node->data << " ";
+        curr_node = curr_node->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head=NULL,*tail=NULL;
+
+    int val;
+    while(true)
+    {
+      cin>>val;
+      if(val==-1)
+        break;
+      insert_at_tail(head,tail,val);
+    }
+    print_linked_list(head);
+    
+    return 0;
+}
