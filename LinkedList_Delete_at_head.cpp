bb88f0c9de270da65519128f82bc6c8c6034cfbc
Kazi-Rifat <kazirifat223@gmail.com>
2026-07-02 11:55:55 +0600

    LinkedList_Delete_at_head

---
+++ LinkedList_Delete_at_head.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        this->data = val;
+        this->next = NULL;
+    }
+};
+void Delete_at_head(Node* &head)
+{
+  if(head==NULL)
+    return;
+  Node *deleteNode=head;
+  head=head->next;
+  delete deleteNode;
+}
+void insert_at_tail(Node* &head,Node* &tail,int val)
+{
+  Node *newnode=new Node(val);
+  if(head==NULL)
+  {
+    head=newnode;
+    tail=newnode;
+    return;
+  }
+  tail->next=newnode;
+  tail=newnode;
+}
+void print_linked_list(Node *head)
+{
+    Node *curr_node = head;
+    while (curr_node != NULL)
+    {
+        cout << curr_node->data << " ";
+        curr_node = curr_node->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head=NULL,*tail=NULL;
+
+    int val;
+    while(true)
+    {
+      cin>>val;
+      if(val==-1)
+        break;
+      insert_at_tail(head,tail,val);
+    }
+    print_linked_list(head);
+    Delete_at_head(head);
+    print_linked_list(head);
+    return 0;
+}
