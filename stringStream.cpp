a8e597fe5c7271f6ed8c60a2821cb1d460ce3b10
Kazi-Rifat <kazirifat223@gmail.com>
2026-08-13 16:20:13 +0600

    string Stream

---
+++ stringStream.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+  string s;
+  //I am Kazi Rifat
+  getline(cin,s);
+  stringstream ss(s);
+  string word;
+
+  while(ss >> word)
+  {
+    cout<<word<<endl;
+  }
+
+}
